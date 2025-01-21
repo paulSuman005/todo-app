21beeac191b905201ad81b63647c47fcb18181ca
paulSuman005 <sumanpauls568gmail.com>
2025-01-22 01:44:02 +0530

    extracting all function and cleaning in code

--- todo_app/src/components/TodoList/TodoList.jsx
+++ todo_app/src/components/TodoList/TodoList.jsx
@@ -6,6 +6,36 @@ import todoContext from "../context/todoContext";
 function TodoList() {
     const {list, setList} = useContext(todoContext);
 
+    function onChangeStatus(todo, finish){
+        const updatedList = list.map((t) => {
+            if(t.id == todo.id){
+                t.status = finish;
+            }
+            return t;
+        })
+
+        setList(updatedList);
+    }
+
+    function onRemoveTodo(todo){
+        const updatedList = list.filter(t => t.id != todo.id);
+        const newList = updatedList.map((t, index) => ({
+            ...t, id: index+1
+        }))
+        setList(newList);
+    }
+
+    function onEditTodo(todo, data) {
+        const updatedList = list.map((t) => {
+            if(t.id == todo.id){
+                t.todoData=data;
+            }
+            return t;
+        })
+
+        setList(updatedList);
+    }
+
     return (
         <div className="todoListWrapper">
             {list.length > 0 && list.map((todo) => (<Todo 
@@ -13,35 +43,9 @@ function TodoList() {
                                                     todoData={todo} 
                                                     todoStatus={todo.status}
                                                     id={todo.id}
-                                                    changeStatus={(finish) => {
-                                                        const updatedList = list.map((t) => {
-                                                            if(t.id == todo.id){
-                                                                t.status = finish;
-                                                            }
-                                                            return t;
-                                                        })
-
-                                                        setList(updatedList);
-                                                    }}
-
-                                                    removeTodo={() => {
-                                                        const updatedList = list.filter(t => t.id != todo.id);
-                                                        const newList = updatedList.map((t, index) => ({
-                                                            ...t, id: index+1
-                                                        }))
-                                                        setList(newList);
-                                                    }}
-
-                                                    editTodo={(data) => {
-                                                        const updatedList = list.map((t) => {
-                                                            if(t.id == todo.id){
-                                                                t.todoData=data;
-                                                            }
-                                                            return t;
-                                                        })
-
-                                                        setList(updatedList);
-                                                    }}
+                                                    changeStatus={(finish) => onChangeStatus(todo, finish)}
+                                                    removeTodo={() => onRemoveTodo(todo)}
+                                                    editTodo={(data) => onEditTodo(todo, data)}
                                                     
                                                 />))}
         </div>
